a27df6f9a811e288f342ec539f31cc17127c5844
ahmed-jobayer <jobayer320721@.com>
2024-11-17 00:35:41 +0600

    token and seller verification function added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
@@ -8,76 +8,98 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // middleware
-app.use(cors({
-    origin: 'http://localhost:5173',
+app.use(
+  cors({
+    origin: "http://localhost:5173",
     optionsSuccessStatus: 200,
-}));
+  })
+);
 app.use(express.json());
 
 // token varification
 const verifyJWT = (req, res, next) => {
-    co
-}
+  const authorizarion = req.header.authorizarion;
+  if (!authorizarion) {
+    return res.send({ message: "No TOken" });
+  }
+  const token = authorizarion.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_KEY_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.send({ message: "Invalid Token" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// verifySeller
+
+const verifySeller = async (req, res, next) => {
+  const email = req.decoded.email;
+  const user = await userCollection.findOne({ email: email });
+  if (user?.role !== "seller") {
+    return res.send({ message: "Forbidden access" });
+  }
+  next();
+};
 
 // mongodb
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oapnwos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(url, {
-    serverApi:{
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-})
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-const userCollection = client.db('gadgetShop').collection("users")
-const productCollection = client.db('gadgetShop').collection("products")
+const userCollection = client.db("gadgetShop").collection("users");
+const productCollection = client.db("gadgetShop").collection("products");
 
 const dbConnect = async () => {
-    try {
-        client.connect();
-        console.log("Database connected successfully")
+  try {
+    client.connect();
+    console.log("Database connected successfully");
 
-        // get user 
+    // get user
 
-        app.get('/user/:email', async ( req, res) => {
-            const email = req.params.email;
-            const query = {email: email}
-            const user = await userCollection.findOne(query)
-            res.send(user)
-        })
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
 
-        // insert user 
-        
-        app.post("/users", async(req, res) => {
-            const user = req.body;
-            const query = {email : user.email}
-            const existingUser = await userCollection.findOne(query)
+    // insert user
 
-            if (existingUser) {
-                return res.send({message: "User already exists"})
-            }
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
 
-            const result = await userCollection.insertOne(user)
-            res.send(result)
-        })
+      if (existingUser) {
+        return res.send({ message: "User already exists" });
+      }
 
-        // add product
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
-        app.post('/add-products', async(req, res) => {
-            const product = req.body
-            const result = await productCollection.insertOne(product)
-            res.send(result)
-        })
+    // add product
 
+    app.post("/add-products", verifyJWT, verifySeller, async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
 
-    } catch (error) {
-        console.log(error.name, error.message)
-    }
-}
-
-dbConnect()
+dbConnect();
 
 // api
 
@@ -87,20 +109,13 @@ app.get("/", (req, res) => {
 
 // jwt
 
-app.post('/authentication', async(req, res)=> {
-    const userEmail = req.body
-    const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {expiresIn:'10d'});
-    res.send({token})
-})
-
-
-
-
-
-
-
-
-
+app.post("/authentication", async (req, res) => {
+  const userEmail = req.body;
+  const token = jwt.sign(userEmail, process.env.ACCESS_KEY_TOKEN, {
+    expiresIn: "10d",
+  });
+  res.send({ token });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
